1978eb17e183f4353d677c1d7a247b435b2bb357
pradhuman22 <shresthapradhuman2020@gmail.com>
2026-08-11 15:27:11 +0900

    Added eslint-config-prettier and prettier-plugin-tailwindcss to improve code formatting and style consistency.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,26 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettier from "eslint-plugin-prettier";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+  eslintConfigPrettier,
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "prettier/prettier": "error",
+    },
+  },
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
